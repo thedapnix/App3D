e8c2813e82a9feef60dcacb21a0dafa58bbf3151
Niclas Andersson <niclas-andersson@outlook.com>
2023-05-31 19:10:52 +0200

    Actually you know what, I'm going to attempt it both ways still, because I'm stupid. Asked homie for book recommendation

--- App3D/PixelShader.hlsl
+++ App3D/PixelShader.hlsl
@@ -11,7 +11,24 @@ struct PixelShaderInput
     float4 worldPosition : TEXCOORD2;
 };
 
+struct SpotLight
+{
+    matrix view;
+    matrix proj;
+    float3 colour;
+    float3 origin;
+    float3 direction;
+};
+
+StructuredBuffer<SpotLight> spotlights : register(t1);
+
 float4 main(PixelShaderInput input) : SV_TARGET
 {
-    return float4(tex2D.Sample(samplerState, input.uv));
+    float4 ambient = float4(0.25f, 0.25f, 0.25f, 0.25f);
+    
+    //Page 504 of the book "Practical Rendering and Computation with Direct3D11", 
+    
+    float4 base = tex2D.Sample(samplerState, input.uv);
+    float4 result = base * ambient;
+    return result;
 }
